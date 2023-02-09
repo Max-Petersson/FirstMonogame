5193f5c6c3031bdd39580ebf28e353bc83f47530
Risalord <max.hej777@gmail.com>
2023-02-09 12:54:19 +0100

    started with board

---
+++ Board.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+public class Board
+{
+	private readonly Texture2D ground;
+	private readonly Texture2D goal;
+	//constants
+	public const char PLAYER = 'P';
+	public const char GROUND = 'X';
+	public const char GOAL = 'G';
+	public const char HOLE = 'H';
+
+	char[,] level;
+
+	
+
+    public Board(ContentManager content)
+    {
+		
+    }
+    public void LoadLevel(char[,] level)
+	{
+
+	}
+	
+	
+}
