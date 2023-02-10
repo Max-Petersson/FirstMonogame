1be6f0696e5447cc6ab031d5d6101a6584340c15
Risalord <max.hej777@gmail.com>
2023-02-10 11:47:37 +0100

    Update BoardObject.cs

--- BoardObject.cs
+++ BoardObject.cs
@@ -1,6 +1,14 @@
-﻿namespace first_mono_game;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Numerics;
+
+namespace first_mono_game;
 
 public abstract class BoardObject
 {
-    
+    Vector2 truePosition;
+    Vector2 DrawPosition;
+
+    Texture2D Texture2Dtexture;
+
+
 }
